d4f7055bf4aa53f7a0c14d59ef4d863bb3cd68c6
w6ye <w6ye@ucsd.edu>
2018-05-04 16:46:43 -0700

    fix tf session error

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-
 import re
-from monty.serialization import loadfn
 from flask import render_template, make_response, request, Flask
 import garnet
 import perovskite
+import tensorflow as tf
 
 app = Flask(__name__)
+
 
 def html_formula(f):
     return re.sub(r"([\d.]+)", r"<sub>\1</sub>", f)
 
+
 @app.route('/', methods=['GET'])
 def index():
     return make_response(render_template('index.html'))
+
 
 @app.route('/query', methods=['GET'])
 def query():
@@ -47,32 +49,36 @@ def query():
         species = {"a": a_composition, "d": d_composition, "c": c_composition}
 
         if abs(charge) < 0.1:
-            model, scaler = garnet.load_model_and_scaler(mix_site) if mix_site else garnet.load_model_and_scaler("c")
-            inputs = garnet.get_descriptor_ext(species)
-            form_e = garnet.get_form_e_ext(inputs, model, scaler)
-            tot_e = garnet.get_tote(form_e, species)
-            if mix_site:
-                decompose_entries = garnet.get_decomposed_entries(species)
-                decomp, ehull = garnet.get_ehull(tot_e=tot_e, species=species,
-                                          unmix_entries=decompose_entries)
-            else:
-                decomp, ehull = garnet.get_ehull(tot_e=tot_e, species=species)
-            formula = garnet.spe2form(species)
-            message = ["<i>E<sub>f</sub></i> = %.3f eV/fu" % form_e,
-                       "<i>E<sub>hull</sub></i> = %.0f meV/atom" %
-                       (ehull * 1000)]
-            if ehull > 0:
-                reaction = []
-                for k, v in decomp.items():
-                    comp = k.composition
-                    comp, f = comp.get_reduced_composition_and_factor()
-                    reaction.append(
-                        '%.3f <a href="https://www.materialsproject.org/materials/%s">%s</a>'
-                        % (v * f / comp.num_atoms * 20, k.entry_id,
-                           html_formula(comp.reduced_formula)))
-                message.append("Decomposition: " + " + ".join(reaction))
+            with tf.Session() as sess:
+                model, scaler, graph = garnet.load_model_and_scaler(mix_site) if mix_site \
+                    else garnet.load_model_and_scaler("c")
+                inputs = garnet.get_descriptor_ext(species)
 
-            message = "<br>".join(message)
+                with graph.as_default():
+                    form_e = garnet.get_form_e_ext(inputs, model, scaler)
+                tot_e = garnet.get_tote(form_e, species)
+                if mix_site:
+                    decompose_entries = garnet.get_decomposed_entries(species)
+                    decomp, ehull = garnet.get_ehull(tot_e=tot_e, species=species,
+                                                     unmix_entries=decompose_entries)
+                else:
+                    decomp, ehull = garnet.get_ehull(tot_e=tot_e, species=species)
+                formula = garnet.spe2form(species)
+                message = ["<i>E<sub>f</sub></i> = %.3f eV/fu" % form_e,
+                           "<i>E<sub>hull</sub></i> = %.0f meV/atom" %
+                           (ehull * 1000)]
+                if ehull > 0:
+                    reaction = []
+                    for k, v in decomp.items():
+                        comp = k.composition
+                        comp, f = comp.get_reduced_composition_and_factor()
+                        reaction.append(
+                            '%.3f <a href="https://www.materialsproject.org/materials/%s">%s</a>'
+                            % (v * f / comp.num_atoms * 20, k.entry_id,
+                               html_formula(comp.reduced_formula)))
+                    message.append("Decomposition: " + " + ".join(reaction))
+
+                message = "<br>".join(message)
         else:
             message = "Not charge neutral! Total charge = %.0f" % charge
     except Exception as ex:
@@ -86,76 +92,11 @@ def query():
     )
     )
 
-# def profile():
-#     try:
-#         c_string = "Ca2+"
-#         a_string = "Al3+"
-#         d_string = "Si4+"
-#         formula = ""
-#
-#         c_composition = garnet.parse_composition(c_string, "C")
-#         a_composition = garnet.parse_composition(a_string, "A")
-#         d_composition = garnet.parse_composition(d_string, "D")
-#
-#         charge = -2.0 * 12
-#
-#         for k in c_composition.keys():
-#             charge += 3 * k.oxi_state * c_composition.get_atomic_fraction(k)
-#         for k in a_composition.keys():
-#             charge += 2 * k.oxi_state * a_composition.get_atomic_fraction(k)
-#         for k in d_composition.keys():
-#             charge += 3 * k.oxi_state * d_composition.get_atomic_fraction(k)
-#
-#         if len(c_composition) > 1:
-#             mix_site = "c"
-#         elif len(a_composition) > 1:
-#             mix_site = "a"
-#         elif len(d_composition) > 1:
-#             mix_site = "d"
-#         else:
-#             mix_site = None
-#
-#         species = {"a": a_composition, "d": d_composition,
-#                    "c": c_composition}
-#
-#         if abs(charge) < 0.1:
-#             model, scaler = garnet.load_model_and_scaler(
-#                 mix_site) if mix_site else garnet.load_model_and_scaler("c")
-#             inputs = garnet.get_descriptor_ext(species)
-#             form_e = garnet.get_form_e_ext(inputs, model, scaler)
-#             tot_e = garnet.get_tote(form_e, species)
-#             if mix_site:
-#                 decompose_entries = garnet.get_decomposed_entries(species)
-#                 decomp, ehull = garnet.get_ehull(tot_e=tot_e, species=species,
-#                                           unmix_entries=decompose_entries)
-#             else:
-#                 decomp, ehull = garnet.get_ehull(tot_e=tot_e, species=species)
-#             formula = garnet.spe2form(species)
-#             message = ["<i>E<sub>f</sub></i> = %.3f eV/fu" % form_e,
-#                        "<i>E<sub>hull</sub></i> = %.0f meV/atom" %
-#                        (ehull * 1000)]
-#             if ehull > 0:
-#                 reaction = []
-#                 for k, v in decomp.items():
-#                     comp = k.composition
-#                     rcomp, f = comp.get_reduced_composition_and_factor()
-#                     reaction.append(
-#                         '%.3f <a href="https://www.materialsproject.org/materials/%s">%s</a>'
-#                         % (v * f / comp.num_atoms * 20, k.entry_id,
-#                            html_formula(comp.reduced_formula)))
-#                 message.append("Decomposition: " + " + ".join(reaction))
-#
-#             message = "<br>".join(message)
-#         else:
-#             message = "Not charge neutral! Total charge = %.0f" % charge
-#     except Exception as ex:
-#         message = str(ex)
-#
-#     print(message)
 
 @app.route('/perovskite', methods=['GET'])
 def perovskite_index():
     return make_response(render_template('index_perov.html'))
+
 
 @app.route('/perovskite_query')
 def perovskite_query():
@@ -184,35 +125,38 @@ def perovskite_query():
         species = {"a": a_composition, "b": b_composition}
 
         if abs(charge) < 0.1:
-            model, scaler = perovskite.load_model_and_scaler(mix_site) if mix_site else perovskite.load_model_and_scaler("a")
-            inputs = perovskite.get_descriptor_ext(species)
-            form_e = perovskite.get_form_e_ext(inputs, model, scaler)
-            # form_e predicted from model is always in /atom
-            # the get_tote func always returns the tote with in /standard fu
-            # which is A2B2O6, 10 atoms
-            tot_e = perovskite.get_tote(form_e * 10, species)
-            if mix_site:
-                decompose_entries = perovskite.get_decomposed_entries(species)
-                decomp, ehull = perovskite.get_ehull(tot_e=tot_e, species=species,
-                                          unmix_entries=decompose_entries)
-            else:
-                decomp, ehull = perovskite.get_ehull(tot_e=tot_e, species=species)
-            formula = perovskite.spe2form(species)
-            message = ["<i>E<sub>f</sub></i> = %.3f eV/fu" % (form_e * 10),
-                       "<i>E<sub>hull</sub></i> = %.0f meV/atom" %
-                       (ehull * 1000)]
-            if ehull > 0:
-                reaction = []
-                for k, v in decomp.items():
-                    comp = k.composition
-                    rcomp, f = comp.get_reduced_composition_and_factor()
-                    reaction.append(
-                        '%.3f <a href="https://www.materialsproject.org/materials/%s">%s</a>'
-                        % (v * f / comp.num_atoms * 10, k.entry_id,
-                           html_formula(comp.reduced_formula)))
-                message.append("Decomposition: " + " + ".join(reaction))
+            with tf.Session() as sess:
+                model, scaler, graph = perovskite.load_model_and_scaler(mix_site) if mix_site \
+                    else perovskite.load_model_and_scaler("a")
+                inputs = perovskite.get_descriptor_ext(species)
+                with graph.as_default():
+                    form_e = perovskite.get_form_e_ext(inputs, model, scaler)
+                # form_e predicted from model is always in /atom
+                # the get_tote func always returns the tote with in /standard fu
+                # which is A2B2O6, 10 atoms
+                tot_e = perovskite.get_tote(form_e * 10, species)
+                if mix_site:
+                    decompose_entries = perovskite.get_decomposed_entries(species)
+                    decomp, ehull = perovskite.get_ehull(tot_e=tot_e, species=species,
+                                                         unmix_entries=decompose_entries)
+                else:
+                    decomp, ehull = perovskite.get_ehull(tot_e=tot_e, species=species)
+                formula = perovskite.spe2form(species)
+                message = ["<i>E<sub>f</sub></i> = %.3f eV/fu" % (form_e * 10),
+                           "<i>E<sub>hull</sub></i> = %.0f meV/atom" %
+                           (ehull * 1000)]
+                if ehull > 0:
+                    reaction = []
+                    for k, v in decomp.items():
+                        comp = k.composition
+                        rcomp, f = comp.get_reduced_composition_and_factor()
+                        reaction.append(
+                            '%.3f <a href="https://www.materialsproject.org/materials/%s">%s</a>'
+                            % (v * f / comp.num_atoms * 10, k.entry_id,
+                               html_formula(comp.reduced_formula)))
+                    message.append("Decomposition: " + " + ".join(reaction))
 
-            message = "<br>".join(message)
+                message = "<br>".join(message)
         else:
             message = "Not charge neutral! Total charge = %.0f" % charge
     except Exception as ex:
@@ -250,10 +194,9 @@ def main():
     app.run(debug=args.debug, host=args.host, port=args.port)
 
 
-
-
 if __name__ == "__main__":
     main()
+
     #
     # import cProfile, pstats, os
     #
