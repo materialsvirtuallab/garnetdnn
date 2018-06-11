2bb25c7199a3ceb27bc23538ff392b5fbe71bd45
w6ye <w6ye@ucsd.edu>
2018-06-11 09:12:29 -0700

    merge garnet and perovskite into one file

---
+++ ehull.py
@@ -0,0 +1,221 @@
+import os
+from collections import Counter
+
+from monty.serialization import loadfn
+from pymatgen import MPRester
+from pymatgen.analysis.phase_diagram import PhaseDiagram
+from pymatgen.analysis.structure_matcher import StructureMatcher
+
+from pymatgen.analysis.structure_matcher import ElementComparator
+from pymatgen.core import Composition
+from pymatgen.entries.compatibility import MaterialsProjectCompatibility
+from pymatgen.entries.computed_entries import ComputedEntry
+
+from pymatgen.io.vasp.sets import _load_yaml_config
+
+from formation_energy import get_descriptor, get_form_e, get_tote
+from util import load_model_and_scaler, spe2form
+
+CONFIG = _load_yaml_config("MPRelaxSet")
+LDAUU = CONFIG["INCAR"]['LDAUU']['O']
+m = MPRester("xNebFpxTfLhTnnIH")
+
+STD_FORMULA = {'garnet': Composition("C3A2D3O12"),
+               "perovskite": Composition("A2B2O6")}
+SITES = {'garnet': ['c', 'a', 'd'],
+         'perovskite': ['a', 'b']}  # use list to preserve order
+SITE_INFO = {'garnet': {'c': {"num_atoms": 3, "max_ordering": 20, "cn": "VIII"},
+                        'a': {"num_atoms": 2, "max_ordering": 7, "cn": "VI"},
+                        'd': {"num_atoms": 3, "max_ordering": 18, "cn": "IV"}},
+             'perovskite': {'a': {"num_atoms": 2, "max_ordering": 10, 'cn': "XII"},
+                            'b': {"num_atoms": 2, "max_ordering": 10, 'cn': "VI"}}}
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+GARNET_CALC_ENTRIES_PATH = os.path.join(DATA_DIR, "garnet/garnet_calc_entries.json")
+GARNET_CALC_ENTRIES = loadfn(GARNET_CALC_ENTRIES_PATH)
+PEROVSKITE_CALC_ENTRIES_PATH = os.path.join(DATA_DIR, "perovskite/perov_calc_entries.json")
+PEROVSKITE_CALC_ENTRIES = loadfn(PEROVSKITE_CALC_ENTRIES_PATH)
+CALC_ENTRIES = {'garnet': GARNET_CALC_ENTRIES,
+                'perovskite': PEROVSKITE_CALC_ENTRIES}
+
+MATCHER = None
+PROTO = None
+
+
+def get_decomposed_entries(structure_type, species, oxides_table_path):
+    """
+    Get decomposed entries for mix types
+    Args:one
+        species (dict): species in dictionary.
+        structure_type(str): garnet or perovskite
+
+    Returns:
+        decompose entries(list):
+            list of entries prepared from unmix
+            garnets/perovskite decomposed from input mix
+            garnet/perovskite
+    """
+
+    def decomposed(specie_complex):
+        """Decompose those have sub-dict to individual dict objects."""
+        for site, specie in specie_complex.items():
+            spe_copy = specie_complex.copy()
+            if len(specie) > 1:
+                for spe, amt in specie.items():
+                    spe_copy[site] = {spe: 1}
+                    yield spe_copy
+
+    decompose_entries = []
+    model, scaler, graph = load_model_and_scaler(structure_type, "unmix")
+    std_formula = STD_FORMULA[structure_type]
+    for unmix_species in decomposed(species):
+        charge = sum([spe.oxi_state * amt * SITE_INFO[structure_type][site]["num_atoms"]
+                      for site in SITE_INFO[structure_type].keys()
+                      for spe, amt in unmix_species[site].items()])
+        if not abs(charge - 2 * std_formula['O']) < 0.1:
+            continue
+
+        formula = spe2form(structure_type, unmix_species)
+        calc_entries = [entry for entry in CALC_ENTRIES[structure_type] if \
+                        entry.name == Composition(formula).reduced_formula]
+        if calc_entries:
+            for entry in calc_entries:
+                decompose_entries.append(entry)
+
+        else:
+            descriptors = get_descriptor(structure_type, unmix_species)
+            with graph.as_default():
+                form_e = get_form_e(descriptors, model, scaler)
+            # tot_e = get_tote(form_e * std_formula.num_atoms, unmix_species)
+            tot_e = get_tote(structure_type, form_e * std_formula.num_atoms, unmix_species, oxides_table_path)
+            entry = prepare_entry(structure_type, tot_e, unmix_species)
+            compat = MaterialsProjectCompatibility()
+            entry = compat.process_entry(entry)
+            decompose_entries.append(entry)
+
+    return decompose_entries
+
+
+def prepare_entry(structure_type, tot_e, species):
+    """
+    Prepare entries from total energy and species.
+
+    Args:
+        tot_e (float): total energy in eV/f.u.
+        species (dict): species in dictionary.
+
+    Returns:
+        ce (ComputedEntry)
+    """
+
+    formula = spe2form(structure_type, species)
+    composition = Composition(formula)
+    elements = [el.name for el in composition]
+
+    potcars = ["pbe %s" % CONFIG['POTCAR'][el] for el in elements]
+
+    parameters = {"potcar_symbols": list(potcars),
+                  "oxide_type": 'oxide'}
+
+    for el in elements:
+        if el in LDAUU:
+            parameters.update({"hubbards": {el: LDAUU[el]}})
+
+    ce = ComputedEntry(composition=composition, energy=0, parameters=parameters)
+    ce.uncorrected_energy = tot_e
+    compat = MaterialsProjectCompatibility()
+    ce = compat.process_entry(ce)
+
+    return ce
+
+
+def filter_entries(structure_type, all_entries, species, return_removed=False):
+    """
+    Filter out the entry with exact same structure as queried entry among the
+    entries in queried chemical space obtained from Materials Project.
+
+    Used Pymatgen.analysis.structure_matcher.fit_anonymous to match the prototype
+    of give structures.
+
+    Args:
+         all_entries (list): entries in queried chemical space obtained from Materials Project
+         composition (Composition): composition of queried entry
+         return_removed (bool): If True, return the filtered entries
+
+    Returns:
+        filtered_entries (list)
+    """
+    global MATCHER, PROTO
+    if not MATCHER:
+        MATCHER = StructureMatcher(ltol=0.2, stol=0.3, angle_tol=5, primitive_cell=True,
+                                   comparator=ElementComparator())
+
+    if not PROTO:
+        PROTO = {"garnet": m.get_structure_by_material_id("mp-3050").get_primitive_structure(),
+                 "perovskite": m.get_structure_by_material_id("mp-4019").get_primitive_structure()}
+        a_sites = [60, 61, 62, 63, 64, 65, 66, 67]
+        for site_ind in a_sites:
+            PROTO['garnet'].replace(site_ind, {"Ga": 1})
+
+    P = PROTO[structure_type].copy()
+    if structure_type == 'garnet:':
+        P.replace_species({"Y": species['c'], "Ga": species['a'], "Al": species['d']})
+    elif structure_type == 'perovskite':
+        P.replace_species({"Ca": species['a'], "Ti": species['b']})
+    composition = Composition(spe2form(structure_type, species))
+    if not return_removed:
+        return [e for e in all_entries \
+                if e.name != composition.reduced_formula \
+                    and not MATCHER.fit(e.structure, P)]
+    else:
+        removed = [e for e in all_entries \
+                   if e.name == composition.reduced_formula \
+                    and MATCHER.fit(e.structure, P)]
+        return removed, [e for e in all_entries if e not in removed]
+
+
+def get_ehull(structure_type, tot_e, species,
+              unmix_entries=None, all_entries=None,
+              debug=False):
+    """
+    Get Ehull predicted under given total energy and species. The composition
+    can be either given by the species dict(for garnet only) or a formula.
+
+    Args:
+        tot_e (float): total energy, the unit is in accordance with given
+            composition.
+        species (dict): species in dictionary.
+        unmix_entries (list): additional list of unmix entries.
+
+    Returns:
+        ehull (float): energy above hull.
+    """
+    formula = spe2form(structure_type, species)
+    composition = Composition(formula)
+    unmix_entries = [] if unmix_entries is None else unmix_entries
+
+    if not all_entries:
+        all_entries = m.get_entries_in_chemsys([el.name for el in composition],
+                                               inc_structure=True)
+    all_entries = filter_entries(structure_type, all_entries, species)
+
+    all_calc_entries = [e for e in CALC_ENTRIES[structure_type]
+                        if set(e.composition).issubset(set(composition)) \
+                        and e.name != composition.reduced_formula]
+
+    if all_calc_entries:
+        all_entries = all_entries + all_calc_entries
+
+    compat = MaterialsProjectCompatibility()
+    all_entries = compat.process_entries(all_entries)
+
+    if not all_entries:
+        raise ValueError("Incomplete")
+    entry = prepare_entry(structure_type, tot_e, species)
+
+    if debug:
+        return entry, all_entries
+
+    phase_diagram = PhaseDiagram(all_entries + [entry] + unmix_entries)
+
+    return phase_diagram.get_decomp_and_e_above_hull(entry)
---
+++ formation_energy.py
@@ -0,0 +1,190 @@
+import os
+from collections import Counter
+
+import numpy as np
+from monty.serialization import loadfn
+from pymatgen import Composition
+from pymatgen.core.periodic_table import get_el_sp
+
+from garnetdnn.util import spe2form
+from pymatgen import MPRester
+
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
+BINARY_OXIDES_PATH = os.path.join(DATA_DIR, "binary_oxide_entries.json")
+BINARY_OXDIES_ENTRIES = loadfn(BINARY_OXIDES_PATH)
+STD_FORMULA = {'garnet': Composition("C3A2D3O12"),
+               "perovskite": Composition("A2B2O6")}
+SITES = {'garnet': ['c', 'a', 'd'],
+         'perovskite': ['a', 'b']}  # use list to preserve order
+SITE_INFO = {'garnet': {'c': {"num_atoms": 3, "max_ordering": 20, "cn": "VIII"},
+                        'a': {"num_atoms": 2, "max_ordering": 7, "cn": "VI"},
+                        'd': {"num_atoms": 3, "max_ordering": 18, "cn": "IV"}},
+             'perovskite': {'a': {"num_atoms": 2, "max_ordering": 10, 'cn': "XII"},
+                            'b': {"num_atoms": 2, "max_ordering": 10, 'cn': "VI"}}}
+
+m = MPRester("VIqD4QUxH6wNpyc5")
+
+
+def binary_encode(config, tot_configs):
+    """
+    Args:
+        config(int): the index of the configuration
+        tot_configs(int): total number of configurations
+    """
+    get_bin = lambda x: format(x, 'b')
+    vb = get_bin(config)
+    max_digit = len([i for i in (bin(tot_configs))[2:] if i.isdigit()])
+    letter = [int(char) for char in vb]
+    letter = [0] * (max_digit - len(letter)) + letter
+    return letter
+
+
+def _raw_input(input_spe, cn_specific, site_info):
+    """
+    inputs w/o configuration encoded
+    """
+    if cn_specific:
+        descriptors = [(get_el_sp(spe).get_shannon_radius(cn=site_info[site]['cn']),
+                        get_el_sp(spe).X)
+                       for spe, site in input_spe]
+    else:
+        descriptors = [(get_el_sp(spe).ionic_radius, get_el_sp(spe).X) \
+                       for spe, site in input_spe]
+    return list(sum(descriptors, ()))
+
+def get_descriptor(structure_type, species, cn_specific=True, unmix_expansion=None, config=None):
+    """
+    Prepare the inputs for model prediction.
+    i.e. extract the
+    [rC', XC', rC'', XC'',rA, XA, rD, XD, b1, b2, b3, b4, b5]
+    inputs array for given species.
+
+    Args:
+        structure_type (str): 'garnet' or 'perovskite'
+        species (dict): species in dictionary.
+        cn_specific(bool): True if use cn specific ionic radius
+        unmix_expansion(list): list the order of sites that reorgonize unmix data
+            eg. for an unmix garnet Ca3Al2Ga3O12, to obtain the corresponding
+            descriptors for extended c-mix/a-mix/d-mix model
+            the unmix_expansion can be specified as
+            ['c', 'c', 'a', 'd'],
+            ['c', 'a', 'a', 'd'],
+            ['c', 'a', 'd', 'd'],
+            respectively
+
+    Returns:
+        inputs (list): numerical inputs of the input structure
+    """
+
+    site_info = SITE_INFO[structure_type]
+    sites = SITES[structure_type]
+    input_spe = [(spe, site) for site in sites
+                 for spe in sorted(species[site],
+                                   key=lambda x: species[site][x])]
+
+    mix_site = [site for site in sites if len(species[site]) == 2]
+    if not mix_site:
+        if not unmix_expansion:
+            return _raw_input(input_spe, cn_specific, site_info)
+        else:
+            sites = unmix_expansion
+            mix_site = Counter(unmix_expansion).most_common(1)[0][0]
+            input_spe = [(spe, site) for site in sites
+                         for spe in sorted(species[site],
+                                           key=lambda x: species[site][x])]
+            max_ordering = site_info[mix_site]['max_ordering']
+            descriptors = _raw_input(input_spe, cn_specific, site_info)
+            descriptors_config = [descriptors + binary_encode(config, max_ordering)
+                                  for config in range(0, max_ordering)]
+            return descriptors_config
+
+
+    else:
+        mix_site = mix_site[0]
+        if len(set(species[mix_site].values())) > 1:
+            descriptors = _raw_input(input_spe, cn_specific, site_info)
+            max_ordering = site_info[mix_site]['max_ordering']
+            if config != None:
+                descriptors_config = descriptors + binary_encode(config, max_ordering)
+            else:
+                descriptors_config = [descriptors + binary_encode(config, max_ordering)
+                                      for config in range(0, max_ordering)]
+            return descriptors_config
+
+        else:
+            mix_site = mix_site[0]
+            max_ordering = site_info[mix_site]['max_ordering']
+            input_spe = [(spe, site) for site in sites
+                         for spe in sorted(species[site],
+                                           key=lambda x: x.__str__())]
+            descriptors = _raw_input(input_spe, cn_specific, site_info)
+            input_spe_r = [(spe, site) for site in sites
+                           for spe in sorted(species[site],
+                                             key=lambda x: x.__str__(),
+                                             reverse=True)]
+            descriptors_r = _raw_input(input_spe_r, cn_specific, site_info)
+            if config != None:
+                descriptors_config = [descriptors + binary_encode(config, max_ordering)]
+                descriptors_config_r = [descriptors_r + binary_encode(config, max_ordering)]
+            else:
+                descriptors_config = [descriptors + binary_encode(config, max_ordering)
+                                      for config in range(0, max_ordering)]
+                descriptors_config_r = [descriptors_r + binary_encode(config, max_ordering)
+                                        for config in range(0, max_ordering)]
+            return descriptors_config + descriptors_config_r
+
+
+def get_form_e(descriptors, model, scaler, return_full=False):
+    """
+    Get formation energy from the given inputs.
+
+    Args:
+        descriptors (dict): input descriptors dict.
+        model (keras.model): keras model object.
+        scaler (keras.StandardScaler): keras StandardScaler object
+        return_full (bool): if True return the full list of energies
+                            instead of the lowest
+    Returns:
+        predicted_ef (float): the predicted formation Energy.
+    """
+    if len(np.array(descriptors).shape) == 1:
+        descriptors = np.array(descriptors).reshape(1, -1)
+    inputs_ext_scaled = scaler.transform(descriptors)
+    if return_full:
+        return model.predict(inputs_ext_scaled)
+    else:
+        form_e = min(model.predict(inputs_ext_scaled))[0]
+        return form_e
+
+
+def get_tote(structure_type, form_e, species, oxides_table_path, debug=False):
+    # formula = spe2form(structure_type, species)
+    # composition = Composition(formula)
+    spe_dict = Counter({})
+    for site in SITE_INFO[structure_type]:
+        spe_dict += Counter({spe.__str__(): round(SITE_INFO[structure_type][site]['num_atoms'] \
+                                                  * species[site][spe]) \
+                             for spe in sorted(species[site], key=lambda x: species[site][x])})
+    composition = Composition(spe_dict)
+    tote = form_e
+
+    for el, amt in composition.items():
+        if debug:
+            print(el)
+        stable_ox_entry = None
+        if el.symbol == 'O':
+            continue
+        if el.symbol in BINARY_OXDIES_ENTRIES:
+            stable_ox_entry = BINARY_OXDIES_ENTRIES[el.symbol]
+
+        if not stable_ox_entry:
+            stable_ox_table = loadfn(oxides_table_path)
+            stable_ox_id = stable_ox_table[el.__str__()]['mpid']
+            stable_ox_entry = m.get_entry_by_material_id(
+                stable_ox_id,
+                property_data=['e_above_hull',
+                               'formation_energy_per_atom'])
+        min_e = stable_ox_entry.uncorrected_energy
+        amt_ox = stable_ox_entry.composition[el.name]
+        tote += (amt / amt_ox) * min_e
+    return tote
